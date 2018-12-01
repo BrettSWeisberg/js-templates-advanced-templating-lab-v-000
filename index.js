ec6abcd8bebe93b2224270a8daeb08fcb1677746
BrettSWeisberg <weisberg.brett@gmail.com>
2018-12-01 15:45:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  let template = document.getElementById("recipe-form-template").innerHTML;
+  let templateFn = Handlebars.compile(template);
+  let html = templateFn(recipe);
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+let recipe = {
+  description: 'yummy chicken noodle soup',
+  ingredients: [
+    {quantity: "1 cup", name: 'chicken'},
+    {quantity: "3 nanoliters", name: 'stock'},
+    {quantity: "12", name: 'noodles'}
+  ]
+}
